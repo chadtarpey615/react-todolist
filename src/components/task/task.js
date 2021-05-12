8aff2cdcd491e0cb5a483e9347a6bc781a6b0fd6
Chad Tarpey <chadtarpey@gmail.com>
2021-05-12 13:51:59 -0500

    created async function to get task from json.server

--- src/components/task/task.js
+++ src/components/task/task.js
@@ -1,18 +1,33 @@
-import React, { useEffect } from 'react'
-import { tasks } from "../../utils/task"
+import React, { useState, useEffect } from 'react'
+// import { tasks } from "../../utils/task"
 
 
 
 export default function Task() {
-
+    const [task, setTask] = useState([])
     useEffect(() => {
-        console.log(tasks)
-    })
+        getData();
+        console.log(task)
+    }, [])
+
+
+    const getData = async () => {
+        const apiUrl = "http://localhost:4000/tasks";
+
+        const response = await fetch(apiUrl).then(data => data.json())
+        setTask(response)
+
+
+    }
     return (
-        <div >
+        <div className="task">
             {
-                tasks.map(task => (
-                    <h1>{task.title}</h1>
+                task.map(task => (
+                    <>
+                        <h3>{task.title}</h3>
+                        <h4>{task.date}</h4>
+                        <h4>{task.task}</h4>
+                    </>
                 ))
             }
         </div>
